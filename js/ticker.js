6c0b3b912aba9e3955bfff625052f9caca34ba94
Jamie Lemon <jamie.lemon@artifex.com>
2023-06-28 16:38:30 +0100

    Initial website concept.

---
+++ js/ticker.js
@@ -0,0 +1,36 @@
+let rate = 0.13315865; // the average rate since August 2016
+let intialDate = new Date("2016-08-01");
+
+var initialNumber;
+
+// Now we have downloads at about one every 2.8 seconds
+function startTimer() {
+  var myInterval = setInterval(tick, 2800);
+}
+
+
+function tick(initialRun) {
+
+
+  let el = document.getElementById("ticker");
+
+  // work out the initial number which is around 29 million since August 2016
+  if (initialRun) {
+    const now = new Date();
+    const diffTime = Math.abs(now - intialDate);
+    const diffSeconds = Math.ceil(diffTime / 1000);
+    var calc = diffSeconds * rate;
+    initialNumber = Math.ceil(calc);
+  }
+
+  var num = initialNumber+=1;
+
+  el.innerHTML = numberWithCommas(num);
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+tick(true);
+startTimer();
